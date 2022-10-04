52e2476c541f95e195729e5352d4ac7aa4b54196
LAPTOP-0BE72HIK\afifk <afifkhanab@gmail.com>
2022-10-04 22:37:15 +0530

    Sign Up UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { Routes, Route} from "react-router-dom";
-import Onboarding from './components/Onboarding';
+//import Onboarding from './components/Onboarding';
 import Test from './components/Test';
 import Roles from './components/Roles';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Onboarding />} />
+        <Route path="/" element={<SignUp />} />
         <Route path="role" element={<Roles />} />
         <Route path="signup" element={<Test />} />
         <Route path="admin" element={<Test />} />
