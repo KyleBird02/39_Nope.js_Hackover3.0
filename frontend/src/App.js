fced9b9780c345bf305604fbc187834192571a5d
LAPTOP-0BE72HIK\afifk <afifkhanab@gmail.com>
2022-10-05 01:55:39 +0530

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route} from "react-router-dom";
-//import Onboarding from './components/Onboarding';
+import Onboarding from './components/Onboarding';
 import Test from './components/Test';
 import Roles from './components/Roles';
 import SignUp from './components/SignUp';
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<Onboarding />} />
         <Route path="role" element={<Roles />} />
-        <Route path="signup" element={<Test />} />
+        <Route path="signup" element={<SignUp />} />
         <Route path="admin" element={<Test />} />
         <Route path="organizer" element={<Test />} />
         <Route path="participant" element={<Test />} />
