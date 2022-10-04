f08c072cd468b36bbf02449efccda535fa55e4d5
Kyle <81643823+KyleBird02@users.noreply.github.com>
2022-10-05 01:28:02 +0530

    All Login/Sign Up Screens Done

---
+++ frontend/src/components/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <div className='screen'>
+        <div className='rsvp-img'></div>
+        <div className='location-text'>Mumbai, IN</div>
+        <div className='header'>
+          <p className='title'>Events Near You</p>
+        </div>
+        <div className='main-body'>
+          
+        </div>
+    </div>
+  )
+}
