16ce1881ffc98443869eed539b03dec74e909b4c
LAPTOP-0BE72HIK\afifk <afifkhanab@gmail.com>
2022-10-04 21:18:46 +0530

    Onboarding UI

--- frontend/src/components/Onboarding.js
+++ frontend/src/components/Onboarding.js
@@ -1,7 +1,17 @@
 import React from 'react'
-
 export default function Onboarding() {
   return (
-    <div>Onboarding</div>
+    <div className='screen'>
+        <div className='rsvp-img'></div>
+        <div className='graphic-1'></div>
+        <div className='welcome-text'>
+            Welcome to <br/>
+            <strong>RSVP</strong><br/>
+            Your all in one event hub
+        </div>
+        <button className='button'>
+        Get Started
+        </button>
+    </div>
   )
 }
